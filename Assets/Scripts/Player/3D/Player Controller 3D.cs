8001b133a95c0b58d0b4c7880a46f2bff3020770
OGBlackDiamond <84690305+OGBlackDiamond@users.noreply.github.com>
2023-05-12 13:35:34 -0700

    3D WASD movement

--- Assets/Scripts/Player/3D/Player Controller 3D.cs
+++ Assets/Scripts/Player/3D/Player Controller 3D.cs
@@ -1,21 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController3D : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float sensitivity;
+    [SerializeField] private float moveSpeed, sensitivity;
+    [SerializeField] private InputActionReference movement;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector2 movementInput;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // refreshes the movement input when not rolling
+        movementInput = movement.action.ReadValue<Vector2>();
+    }
+    void FixedUpdate()
+    {
+        // moves 100 times every second
+        regularMovement(movementInput.x, movementInput.y);
+    }
+
+    private void regularMovement(float xValue, float zValue)
+    {
+        var moveDir = new Vector3(xValue, 0, zValue);
+
+        moveDir *= moveSpeed;
+
+        transform.position += moveDir;
     }
 }
